e385c5c46c4dd2fab4cf8c89d559ceb5b9f2c885
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-08-02 16:07:36 -0700

    header started with imgs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 
 import Header from './Header.js';
-import Body from './Body.js';
-import Recipe from './Recipe.js';
-import Footer from './Footer.js';
+import Social from './Header.js';
+
+// import Body from './Body.js';
+// import Recipe from './Recipe.js';
+// import Footer from './Footer.js';
 
 class App extends Component {
   render() { 
     return ( 
       <>
       <Header />
-      <Body />
+      {/* <Body />
       <Recipe />
-      <Footer/>
+      <Footer/> */}
       </>
        );
   }
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import DeliciousLogo from './delicious-logo.png';
+import FbIcon from './fb-icon.png';
+import TwitIcon from './twit-icon.png';
+import GoogleIcon from './gp-icon.png';
+import InstaIcon from './insta-icon.png';
+import FlicIcon from './flic-icon.png';
+import PintIcon from './pint-icon.png';
+import RssIcon from './rss-icon.png';
+import MailIcon from './mail-icon.png';
 
 class Header extends Component {
     render() { 
-        return (<div></div> );
+        return (
+        <>
+            <div id="delicious">
+                <img src={DeliciousLogo} alt='logo' />
+            </div> 
+            <div id="social">
+            <ul>
+                <img src={FbIcon} alt='FB' />
+                <img src={TwitIcon} alt='Twitter' />
+                <img src={GoogleIcon} alt='Google+' />
+                <img src={InstaIcon} alt='Instagram' />
+                <img src={FlicIcon} alt='Flic' />
+                <img src={PintIcon} alt='Pinterest' />
+                <img src={RssIcon} alt='RSS' />
+                <img src={MailIcon} alt='Email' />
+            </ul>
+            </div>
+        </>
+
+        );
     }
 }
  
 export default Header;
+
